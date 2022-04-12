8da6cc5514c6507bef6ed3ffb27fabe1891fa7be
Paul Köhler <koehler_paul@outlook.de>
2022-04-12 07:31:59 +0200

    🚀 Initial Commit

---
+++ android/app/src/main/kotlin/com/koehlermuc/strech_timer/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.koehlermuc.strech_timer
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/views/color_view.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class ColorView extends StatelessWidget {
+  const ColorView({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(
+        child: Text("Test123"),
+      ),
+    );
+  }
+}
